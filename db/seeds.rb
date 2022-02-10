e57972d85b251d363b8568aff3dd7be5f7e2fc1d
Matan Amidor <87947521+Mado13@users.noreply.github.com>
2022-02-10 17:32:41 +0100

    added seeds to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Book.delete_all
+Author.delete_all
+
+tom = Author.create!(name: 'Tom Robbins', deceased: false)
+stephan = Author.create!(name: 'Stephan King', deceased: false)
+roald = Author.create!(name: 'Roald Dahl', deceased: true)
+
+15.times do
+  book = Book.new(
+    title: Faker::Book.title,
+    rating: rand(1..10),
+    publishdate: Faker::Date.between(from: '2014-09-23', to: '2014-09-25'),
+    available: [true, false].shuffle
+  )
+  book.author = [tom, stephan, roald].sample
+  book.save!
+end
