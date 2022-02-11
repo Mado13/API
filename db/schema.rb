791bc2b4ace68d87a616d4c90d67ec42e86fd475
Matan Amidor <87947521+Mado13@users.noreply.github.com>
2022-02-11 10:34:44 +0100

    added username coloum to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_11_084329) do
+ActiveRecord::Schema.define(version: 2022_02_11_093420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2022_02_11_084329) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
   end
 
   add_foreign_key "books", "authors"
